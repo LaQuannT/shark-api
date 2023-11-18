e21c8c258113c2a8a8cf9bf171f7c1f7ed12ed6a
LaQuan <117905467+LaQuannT@users.noreply.github.com>
2023-11-18 08:47:12 +0000

    feat: add shark handlers & register handlers in init handler func

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -3,12 +3,17 @@ package handlers
 import (
 	"database/sql"
 
-	"github.com/LaQuannT/shark-api/repository"
 	"github.com/gin-gonic/gin"
 	"github.com/sirupsen/logrus"
+
+	"github.com/LaQuannT/shark-api/repository"
 )
 
 func InitHandlers(router *gin.Engine, db *sql.DB, log *logrus.Logger) {
-	ur := repository.NewUserRepo(db)
-	RegisterUserHandlers(router,ur,log)
+	usr := repository.NewUserRepo(db)
+	srk := repository.NewSharkRepo(db)
+
+	RegisterUserHandlers(router, usr, log)
+	RegisterSharkHandlers(router, srk, log)
 }
+
---
+++ handlers/shark.handlers.go
@@ -0,0 +1,158 @@
+package handlers
+
+import (
+	"fmt"
+	"net/http"
+	"strconv"
+
+	"github.com/gin-gonic/gin"
+	"github.com/sirupsen/logrus"
+
+	"github.com/LaQuannT/shark-api/repository"
+	"github.com/LaQuannT/shark-api/types"
+)
+
+type SharkHandler struct {
+	Repo   *repository.SharkRepo
+	Logger *logrus.Logger
+}
+
+type Search struct {
+	name string `form:"name"`
+}
+
+func RegisterSharkHandlers(router *gin.Engine, repo *repository.SharkRepo, logger *logrus.Logger) {
+	h := &SharkHandler{Repo: repo, Logger: logger}
+
+	router.GET("/sharks", h.HandleGetSharkByName)
+	router.POST("/sharks", h.HandleCreateShark)
+	router.GET("/sharks/:sharkId", h.HandleGetSharkById)
+	router.PUT("/sharks/:sharkId", h.HandleUpdateShark)
+	router.DELETE("/sharks/:sharkId", h.HandleDeleteShark)
+}
+
+func (h *SharkHandler) HandleCreateShark(c *gin.Context) {
+	var shark types.Shark
+
+	if err := c.ShouldBindJSON(&shark); err != nil {
+		h.Logger.Errorf("SharkHandler/HandleCreateShark/ShouldBindJSON: %v", err)
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	id, err := h.Repo.CreateShark(shark)
+	if err != nil {
+		h.Logger.Errorf("SharkHandler/HandleCreateShark/CreateShark: %v", err)
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	s, err := h.Repo.GetSharkById(id)
+	if err != nil {
+		h.Logger.Errorf("SharkHandler/HandleCreateShark/GetSharkById: %v", err)
+		c.JSON(
+			http.StatusInternalServerError,
+			gin.H{"error": "shark created - error returing shark data"},
+		)
+		return
+	}
+
+	c.JSON(http.StatusCreated, s)
+}
+
+func (h *SharkHandler) HandleGetSharkById(c *gin.Context) {
+	id, err := strconv.Atoi(c.Param("sharkId"))
+	if err != nil {
+		h.Logger.Errorf("SharkHandler/handlerGetSharkById/Atoi: %v", err)
+		c.JSON(
+			http.StatusBadRequest,
+			gin.H{"error": "Invalid shark id param - must be a valid number"},
+		)
+		return
+	}
+
+	s, err := h.Repo.GetSharkById(id)
+	if err != nil {
+		h.Logger.Errorf("SharkHandler/HandleGetSharkById/GetSharkById: %v", err)
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusCreated, s)
+}
+
+func (h *SharkHandler) HandleGetSharkByName(c *gin.Context) {
+	var search Search
+
+	if err := c.ShouldBind(&search); err != nil {
+		h.Logger.Errorf("SharkHandler/HandleGetSharkByName/ShouldBind: %v", err)
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	shark, err := h.Repo.GetSharkByName(search.name)
+	if err != nil {
+		h.Logger.Errorf("SharkHandler/HandleGetSharkByName/GetSharkByName: %v", err)
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, shark)
+}
+
+func (h *SharkHandler) HandleUpdateShark(c *gin.Context) {
+	var shark types.Shark
+	id, err := strconv.Atoi(c.Param("sharkId"))
+	if err != nil {
+		h.Logger.Errorf("SharkHandler/HandleUpdateShark/Atoi: %v", err)
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	if err := c.ShouldBindJSON(&shark); err != nil {
+		h.Logger.Errorf("SharkHandler/HandleUpdateShark/ShouldBindJSON: %v", err)
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	shark.Id = id
+
+	if err := h.Repo.UpdateShark(shark); err != nil {
+		h.Logger.Errorf("SharkHandler/HandleUpdateShark/UpdateShark: %v", err)
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	s, err := h.Repo.GetSharkById(id)
+	if err != nil {
+		h.Logger.Errorf("SharkHandler/HandleUpdateShark/GetSharkById: %v", err)
+		c.JSON(
+			http.StatusInternalServerError,
+			gin.H{"error": "Shark Updated - error returnin shark data"},
+		)
+		return
+	}
+
+	c.JSON(http.StatusOK, s)
+}
+
+func (h *SharkHandler) HandleDeleteShark(c *gin.Context) {
+	id, err := strconv.Atoi(c.Param("sharkId"))
+	if err != nil {
+		h.Logger.Errorf("SharkHandler/HandleDeleteShark/Atoi: %v", err)
+		c.JSON(
+			http.StatusBadRequest,
+			gin.H{"error": "Invalid shark id param - must be a valid number"},
+		)
+		return
+	}
+
+	if err := h.Repo.DeleteShark(id); err != nil {
+		h.Logger.Errorf("SharkHandler/HandleDeleteShark/DeleteShark: %v", err)
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	msg := fmt.Sprintf("Shark %d - successfully deleted", id)
+	c.JSON(http.StatusOK, gin.H{"message": msg})
+}
